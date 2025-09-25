70f38f3e0605a3555e4cc80dd693bc037d00473f
akshatkumarsingh047 <akshatkumarsingh047@gmail.com>
2025-09-25 17:31:41 +0530

    font and back under 5000

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,18 @@
 import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
+import path from "path"
 const app=express();
 dotenv.config();
 const PORT=process.env.PORT||3000
 app.use('/api/auth',authRoutes)
+const __dirname=path.resolve()
+//make ready for deployment
+if(process.env.Node_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.get("*",(_,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
+    })
+}
 
 app.listen(PORT,()=>console.log(`Server is up at ${PORT}`))
