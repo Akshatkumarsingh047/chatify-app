80c8929990235ebd7f07e16a825a17cbedefbd03
akshatkumarsingh047 <akshatkumarsingh047@gmail.com>
2025-09-26 15:55:31 +0530

    frontend completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,13 +3,25 @@ import dotenv from "dotenv"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/messages.route.js"
 import path from "path"
+import cors from "cors"
 import { connectDB } from "./lib/db.js";
 import cookieParser from"cookie-parser"
-const app=express();
+import { ENV } from "./lib/env.js"
+import { app,server } from "./lib/socket.js"
+
 dotenv.config();
 const PORT=process.env.PORT||3000
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(cookieParser())
+app.use(cors({
+    origin:ENV.CLIENT_URL,
+    credentials:true
+}))
+app.options("*", cors({
+  origin: ["http://localhost:5173", ENV.CLIENT_URL],
+  credentials: true
+}));
 app.use('/api/auth',authRoutes)
 app.use('/api/messages',messageRoutes)
 const __dirname=path.resolve()
@@ -21,5 +33,6 @@ if(process.env.NODE_ENV==="production"){
     })
 }
 connectDB().then(()=>{
-    app.listen(PORT,()=>console.log(`Server is up at ${PORT}`))
+    server.listen(PORT,()=>console.log(`Server is up at ${PORT}`))
 }).catch((err)=>console.error(err));
+
