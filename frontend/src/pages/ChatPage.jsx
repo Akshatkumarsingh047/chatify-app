6bae9052eb5879bc833dd9b69c39f1ce2bfe67ea
akshatkumarsingh047 <akshatkumarsingh047@gmail.com>
2025-09-26 17:28:56 +0530

    bug fix

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -15,21 +15,33 @@ function ChatPage() {
     <div className="relative w-full max-w-6xl h-[800px]">
       <BorderAnimatedContainer>
         {/* LEFT SIDE */}
-        <div className={`${selectedUser?"hidden":"flex flex-1 flex-col"} md:flex md:w-80 md:flex-col bg-slate-800/50 backdrop-blur-sm`}>
+        <div
+          className={`
+            flex flex-col bg-slate-800/50 backdrop-blur-sm
+            ${selectedUser ? "hidden" : "flex flex-1"}   // mobile
+            md:flex md:w-80                      // desktop always visible w-80
+          `}
+        >
           <ProfileHeader />
           <ActiveTabSwitch />
-
           <div className="flex-1 p-4 space-y-2 overflow-y-auto">
             {activeTab === "chats" ? <ChatsList /> : <ContactList />}
           </div>
         </div>
 
         {/* RIGHT SIDE */}
-        <div className={`${selectedUser?"":"hidden"} flex flex-col flex-1 bg-slate-900/50 backdrop-blur-sm`}>
+        <div
+          className={`
+            flex flex-col bg-slate-900/50 backdrop-blur-sm
+            ${selectedUser ? "flex" : "hidden"}   // mobile
+            md:flex md:flex-1                     // desktop always visible, flex-1
+          `}
+        >
           {selectedUser ? <ChatContainer /> : <NoConversationPlaceholder />}
         </div>
       </BorderAnimatedContainer>
     </div>
   );
 }
+
 export default ChatPage;
